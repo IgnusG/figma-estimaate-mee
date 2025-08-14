e0662c7de0ddc1c832f9e0583177bcc46f8dc1a2
Jonathan J. (JJ) <j.juhas@housinganywhere.com>
2025-08-14 21:00:43 +0200

    Adjust point descriptions slightly

--- widget-src/utils/constants.ts
+++ widget-src/utils/constants.ts
@@ -10,44 +10,47 @@ export const STORY_POINT_CARDS = [
   },
   {
     value: 0.5,
-    title: "Tiny Change",
+    title: "One liner",
     tooltip: "I'll be finished before you have time to grab coffee",
     assetPath: `${ASSET_BASE_URL}/story-points-0.5.jpg`,
   },
   {
     value: 1,
     title: "Quick Win",
-    tooltip: "Tell PM it's going to take a day and then scroll Instagram for 6 hours",
+    tooltip:
+      "Tell PM it's going to take a day and then scroll Instagram for 4 hours",
     assetPath: `${ASSET_BASE_URL}/story-points-1.jpg`,
   },
   {
     value: 2,
-    title: "Easy Peasy",
-    tooltip: "Half a day should do it",
+    title: "A few unknowns",
+    tooltip: "A day should do it - we know almost everything after all",
     assetPath: `${ASSET_BASE_URL}/story-points-2.jpg`,
   },
   {
     value: 3,
-    title: "Doable",
-    tooltip: "A task a day keeps the manager at bay",
+    title: "Dependencies are piling up",
+    tooltip: "A task every few days keeps the manager at bay",
     assetPath: `${ASSET_BASE_URL}/story-points-3.jpg`,
   },
   {
     value: 5,
     title: "The everything estimate",
-    tooltip: "2-3 days later and it will be just about done - maybe",
+    tooltip:
+      "You start on Monday and on Friday finish off by bringing production down",
     assetPath: `${ASSET_BASE_URL}/story-points-5.jpg`,
   },
   {
     value: 8,
-    title: "Big One",
-    tooltip: "We might be ready by next week?",
+    title: "It's getting difficult",
+    tooltip: "It better be finished before the sprint ends",
     assetPath: `${ASSET_BASE_URL}/story-points-8.jpg`,
   },
   {
     value: 13,
     title: "Heavy Lift",
-    tooltip: "Ladies and gentlement, it's a sprint worth of effort",
+    tooltip:
+      "Ladies and gentlemen, we won't finish this before the sprint ends",
     assetPath: `${ASSET_BASE_URL}/story-points-13.jpg`,
   },
 ];
@@ -55,25 +58,25 @@ export const STORY_POINT_CARDS = [
 export const JOKER_CARDS = [
   {
     value: "∞",
-    title: "Forever",
+    title: "Will take Forever",
     tooltip: "This is way too big to estimate buddy",
     assetPath: `${ASSET_BASE_URL}/special-infinity.jpg`,
   },
   {
     value: "?",
-    title: "I don't know",
-    tooltip: "No idea what's this about?",
+    title: "What is this about?",
+    tooltip: "Still no idea what we are talking about - can't help you",
     assetPath: `${ASSET_BASE_URL}/special-idk.jpg`,
   },
   {
     value: "🍦",
-    title: "Ice Cream!",
+    title: "Time for Ice Cream!",
     tooltip: "Emad is inviting you to an ice cream party",
     assetPath: `${ASSET_BASE_URL}/special-ice-cream.jpg`,
   },
   {
     value: "☕",
-    title: "I need coffee",
+    title: "Coffee please",
     tooltip: "Need a break to think - let's get some coffee",
     assetPath: `${ASSET_BASE_URL}/special-coffee.jpg`,
   },
